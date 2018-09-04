819afb0b74b9df3dc5b3cdd54a203b9169874570
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2018-09-04 21:08:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,62 @@
-
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.MIMEBase import MIMEBase
+import getpass
 from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def enter():
+    ''' Function to get user input email-from/password/email-to/subject/message'''
+    msg = MIMEMultipart()
+    msg['from'] = input("Enter your email: ")
+    password = getpass.getpass('Password: ')
+    msg['to'] = input("Enter the email to: ")
+    msg['subject'] = input("Subject: ")
+    message = input("Message: ")
+
+    return msg, password, message
  
-fromaddr = "YOUR EMAIL"
-toaddr = "EMAIL ADDRESS YOU SEND TO"
+
+def configure(msg, message, password):
+    ''' Configure the SMTP object and login'''
+
+    msg.attach(MIMEText(message, 'plain'))
+    smtp_obj = smtplib.SMTP('smtp.gmail.com: 587')
+    smtp_obj.starttls()
+    smtp_obj.login(msg['from'], password)
+
+    return smtp_obj
+
+def attachment(msg):
+    filename = input("Name of the file and extention: ")
+    attachment = open(input("Path of the file: "), "rb")
+    
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', f"attachment; filename= {filename}")
+    
+    msg.attach(part)
+    return msg
  
-msg = MIMEMultipart()
  
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "SUBJECT OF THE EMAIL"
+def send(msg, smtp_obj):
+    ''' Send the email and quit the SMPP '''
+
+    smtp_obj.sendmail(msg['from'], msg['to'], msg.as_string())
+    smtp_obj.quit()
  
-body = "TEXT YOU WANT TO SEND"
- 
-msg.attach(MIMEText(body, 'plain'))
- 
-filename = "NAME OF THE FILE WITH ITS EXTENSION"
-attachment = open("PATH OF THE FILE", "rb")
- 
-part = MIMEBase('application', 'octet-stream')
-part.set_payload((attachment).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
- 
-msg.attach(part)
- 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(fromaddr, "YOUR PASSWORD")
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
-server.quit()
+    print(f"Successfully sent email to {msg['to']}")
+
+
+def main():
+    with_attach = input("Send email with attachment True or False: ")
+    msg, password, message = enter()
+    smtp_obj = configure(msg, message, password)
+    if  with_attach.lower() == "true": msg = attachment(msg)
+  
+    send(msg, smtp_obj)
+
+
+if __name__ == "__main__":
+    main()
