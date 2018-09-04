38314a8fdb8cbe4bb61d83fc95346900056d64d2
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2018-09-04 18:35:05 +0300

    Update withoutattachment.py

--- withoutattachment.py
+++ withoutattachment.py
@@ -1,22 +1,46 @@
-
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
+import getpass
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def enter():
+    ''' Function to get user input email-from/password/email-to/subject/message'''
+    msg = MIMEMultipart()
+    msg['from'] = input("Enter your email: ")
+    password = getpass.getpass('Password: ')
+    msg['to'] = input("Enter the email to: ")
+    msg['subject'] = input("Subject: ")
+    message = input("Message: ")
+
+    return msg, password, message
+ 
+
+def configure(msg, message, password):
+    ''' Configure the SMTP object and login'''
+
+    msg.attach(MIMEText(message, 'plain'))
+    smtp_obj = smtplib.SMTP('smtp.gmail.com: 587')
+    smtp_obj.starttls()
+    smtp_obj.login(msg['from'], password)
+
+    return smtp_obj
  
  
-fromaddr = "YOUR ADDRESS"
-toaddr = "ADDRESS YOU WANT TO SEND TO"
-msg = MIMEMultipart()
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "SUBJECT OF THE MAIL"
+def send(msg, smtp_obj):
+    ''' Send the email and quit the SMPP '''
+
+    smtp_obj.sendmail(msg['from'], msg['to'], msg.as_string())
+    smtp_obj.quit()
  
-body = "YOUR MESSAGE HERE"
-msg.attach(MIMEText(body, 'plain'))
- 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(fromaddr, "YOUR PASSWORD")
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
-server.quit()
+    print(f"Successfully sent email to {msg['to']}")
+
+
+def main():
+    msg, password, message = enter()
+    smtp_obj = configure(msg, message, password)
+    send(msg, smtp_obj)
+
+
+if __name__ == "__main__":
+    main()
